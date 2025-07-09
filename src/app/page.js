43b40b8b1bea404f1fb87d11b42ce4aca462bcbb
Wistara Banyu Kayana <wisthara.banyu.kayana@gmail.com>
2025-07-10 00:28:29 +0700

    responsiveness

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <main className="flex min-h-screen flex-col items-center justify-center p-12">
       <Image
         src="/logo.png"
         alt="Padelix Logo"
@@ -11,9 +11,8 @@ export default function Home() {
         className="bg-slate-200 rounded-full mb-6 shadow-xl/50 dark:shadow-slate-50 shadow-slate-700 transition-all duration-300 hover:scale-105 transform animate-pulse"
         priority
       />
-      <h1 className="text-4xl font-bold mb-2">
-        Padelix Indonesia dalam Konstruksi
-      </h1>
+
+      <h1 className="text-4xl font-bold mb-2 text-center">Segera Hadir!</h1>
     </main>
   );
 }
